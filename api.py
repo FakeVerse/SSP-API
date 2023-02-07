aa26e17157faf3c8793d27322a372b3767ee69f1
FakeVerse <92539874+FakeVerse@users.noreply.github.com>
2023-02-07 17:56:59 +0100

    Update api.py

--- api.py
+++ api.py
@@ -43,5 +43,7 @@ def run_command():
 
     return("Command Has Been Sent", 200)
 
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
